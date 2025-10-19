47e725a1e2f78e53db2448946eb30ae74dbc5e66
jfmalawat <jfmalawat27@gmail.com>
2025-10-19 16:00:46 +0700

    menambahkan kondisional

--- kalkulator_bmi.py
+++ kalkulator_bmi.py
@@ -2,16 +2,26 @@
 
 print("Perhitungan BMI (Body Mess Index)")
 print("=================================")
-berat = input("Masukkan berat badan (kg) : ")
-berat = float(berat)
-tinggi = input("Masukkan tinggi badan (m) : ")
-tinggi = float(tinggi)
+berat = float(input("Masukkan berat badan (kg) : "))
+tinggi = float(input("Masukkan tinggi badan (m) : "))
 
 bmi = berat/(tinggi**2)
 berat_ideal= dict()
 berat_ideal["bawah"]= 18.5*(tinggi**2)
 berat_ideal["atas"]= 25*(tinggi**2)
 
-print(f"Nilai BMI normal diantara 18.5 - 25 m^2. Hasil nilai bmi anda adalah {bmi:.2f}")
+if bmi < 18.5:
+    kondisi = "Anda kekurangan berat badan :("
+elif bmi <= 25:
+    kondisi = "Anda memiliki berat badan yang normal :)"
+elif bmi <= 30:
+    kondisi = "Anda mengalami kelebihan berat badan :D"
+else:
+    kondisi = "Anda mengalami obesitas"
+
+print("\nHasil Perhitungan Kalkulator BMI")
+print("================================")
+print(f"Hasil nilai bmi anda adalah {bmi:.2f}")
+print(f"{kondisi}")
 print(f"Berat badan ideal anda adalah dalam rentan {berat_ideal['bawah']:.2f} kg - {berat_ideal['atas']:.2f} kg")
 print("Terimakasih telah bersedia menggunakan program ini :)")
