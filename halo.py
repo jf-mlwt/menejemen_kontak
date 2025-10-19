0538cfefdac53ec8b71ac434b56c34e2c4bc27f3
jfmalawat <jfmalawat27@gmail.com>
2025-10-19 19:41:22 +0700

    menambah manajemen kontak

--- halo.py
+++ halo.py
@@ -1,1 +1,4 @@
 print("Nama saya Jana")
+x = [1,2,3]
+for i in x:
+    print(x)
---
+++ manajemen_kontak.py
@@ -0,0 +1,9 @@
+"PROGRAM MANAJEMEN KONTAK"
+
+print("\nMenu Kontak :")
+print("1. Melihat Semua Kontak")
+print("2. Menambah Kontak")
+print("3. Menghapus Kontak")
+print("4. Keluar dari Kontak")
+
+pilihan = input("\nMasukkan pilihan anda (1,2,3 atau 4) :")
